42828484fad093aa115d15cadeaf8af85999b6a0
Morrow3 <joni.hauhia@cs.helsinki.fi>
2013-03-15 14:42:07 +0200

    Update main.py
    näköjään edellinen versio oli siistimätön ja kommentoimaton, joten lisätty em. asioita

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-#tulostaa kytkimen ARP:in.
+#tulostaa lähiverkon ARP:it.
 import arp
+#etsittävien osoitteiden alkuosa
 ip = "192.168.0."
 i = 1
 for i in range(0,24):
+    #lisätään aliosoitteet
     vip = ip
     i +=1
     vip+=str(i)
     print vip
     dee = arp.arp_resolve(vip, strformat=False, source=None)
-    #print dee
     print(arp.mac_straddr(dee, printable=True, delimiter=":"))
